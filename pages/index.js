50d8427e4070e63c4654dd3db688e06e02f73eb0
wheel0ck <wjose14proxy@gmail.com>
2022-04-18 18:41:25 +0200

    Adding Banner component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Banner from '../components/banner'
 
 export default function Home() {
+  const handleOnBannerClick = () => {
+    console.log('click on banner')
+  }
   return (
     <div className={styles.container}>
       <Head>
@@ -11,23 +15,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Banner buttonText="View stores nearby" handleOnClick={handleOnBannerClick} />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
