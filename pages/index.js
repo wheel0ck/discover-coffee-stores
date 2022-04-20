4c767893c63642983a8a599b7f830e467c656546
wheel0ck <wjose14proxy@gmail.com>
2022-04-20 15:11:41 +0200

    Adding font

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <Banner buttonText="View stores nearby" handleOnClick={handleOnBannerClick} />
+        <div className={styles.heroImage}>
+          <Image src="/static/hero-image.png" width={700} height={400} />
+        </div>
       </main>
     </div>
   )
