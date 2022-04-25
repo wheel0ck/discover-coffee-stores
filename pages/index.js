f82e0e8c306719d63fbfb0716801d91170fd9115
wheel0ck <wjose14proxy@gmail.com>
2022-04-25 16:17:22 +0200

    adding data with getStaticprops

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Banner from '../components/banner'
+import Card from '../components/card'
+import coffeeStoresData from '../data/coffee-stores.json'
 
-export default function Home() {
+export async function getStaticProps(context) {
+  return { props: { coffeeStores: coffeeStoresData } }
+}
+
+export default function Home(props) {
   const handleOnBannerClick = () => {
     console.log('click on banner')
   }
@@ -19,6 +25,22 @@ export default function Home() {
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" width={700} height={400} />
         </div>
+        {props.coffeeStores.length > 0 && (
+          <>
+            <h2 className={styles.heading2}>Toronto Stores</h2>
+            <div className={styles.cardLayout}>
+              {props.coffeeStores.map((coffeeStore) => (
+                <Card
+                  key={coffeeStore.id}
+                  name={coffeeStore.name}
+                  imgUrl={coffeeStore.imgUrl}
+                  href={`/coffee-store/${coffeeStore.id}`}
+                  className={styles.card}
+                />
+              ))}
+            </div>
+          </>
+        )}
       </main>
     </div>
   )
