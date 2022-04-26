0bfe35af6c4bcc79652accf70e3de44db4111a8e
wheel0ck <wjose14proxy@gmail.com>
2022-04-26 18:23:51 +0200

    refactor page store

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,25 +1,38 @@
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import Head from 'next/head'
 import coffeeStoresData from '../../data/coffee-stores.json'
 
 export async function getStaticProps({ params }) {
-  return { props: { coffeeStores: coffeeStoresData.find((coffeeStore) => coffeeStore.id.toString() === params.id) } }
+  return { props: { coffeeStore: coffeeStoresData.find((coffeeStore) => coffeeStore.id.toString() === params.id) } }
 }
 
 export function getStaticPaths() {
+  const paths = coffeeStoresData.map((coffeeStore) => {
+    return { params: { id: coffeeStore.id.toString() } }
+  })
+
   return {
-    paths: [{ params: { id: '0' } }, { params: { id: '1' } }],
-    fallback: false,
+    paths,
+    fallback: true,
   }
 }
 
 const CoffeeStore = (props) => {
   const router = useRouter()
+  const { address, name, neighborhood } = props.coffeeStore
+
+  if (router.isFallback) return <div>Loading...</div>
 
   return (
     <div>
+      <Head>
+        <title>{name}</title>
+      </Head>
       <Link href="/">go home page</Link>
-      <p>{props.coffeeStores.address}</p>
+      <p>{address}</p>
+      <p>{name}</p>
+      <p>{neighborhood}</p>
     </div>
   )
 }
