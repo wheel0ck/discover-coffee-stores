8d69a542aae9f68fb102af8922d8b1ae1cba99f9
wheel0ck <wjose14proxy@gmail.com>
2022-04-26 16:50:42 +0200

    adding static path

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,11 +1,25 @@
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import coffeeStoresData from '../../data/coffee-stores.json'
 
-const CoffeeStore = () => {
+export async function getStaticProps({ params }) {
+  return { props: { coffeeStores: coffeeStoresData.find((coffeeStore) => coffeeStore.id.toString() === params.id) } }
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [{ params: { id: '0' } }, { params: { id: '1' } }],
+    fallback: false,
+  }
+}
+
+const CoffeeStore = (props) => {
   const router = useRouter()
+
   return (
     <div>
-      <Link href="/"></Link>coffe page
+      <Link href="/">go home page</Link>
+      <p>{props.coffeeStores.address}</p>
     </div>
   )
 }
